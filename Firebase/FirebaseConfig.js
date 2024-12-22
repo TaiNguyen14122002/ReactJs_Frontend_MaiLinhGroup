5ba6bccb5322f8942760e84b9a432adf36058668
Nguyễn Văn Tài <94286745+TaiNguyen14122002@users.noreply.github.com>
2024-12-23 03:00:16 +0700

    23/12/2024_(2)

--- Firebase/FirebaseConfig.js
+++ Firebase/FirebaseConfig.js
@@ -20,3 +20,15 @@ const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
 export { storage };
+
+// Hàm tải ảnh từ Firebase Storage
+export const getImageUrl = async (path) => {
+  const storageRef = ref(storage, "https://firebasestorage.googleapis.com/v0/b/pms-fe88f.appspot.com/o/files%2FBlack%20and%20White%20Auto%20Repair%20Logo%20(1).png?alt=media&token=5cbbc329-d36f-46e1-8526-53d6e2f11699");
+  try {
+    const url = await getDownloadURL(storageRef);
+    return url;
+  } catch (error) {
+    console.error("Error getting image URL:", error);
+    return null;
+  }
+};
